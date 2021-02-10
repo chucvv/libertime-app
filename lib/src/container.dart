4573a8475a0aeafc21c8fdd06f67945b8e75376a
chucvv <vanchucv@gmail.com>
2021-02-10 16:43:29 +0700

    [Style] Make gradient

--- lib/src/container.dart
+++ lib/src/container.dart
@@ -1,6 +1,7 @@
 import 'package:features/modules.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:share_ui/awesome_ui.dart';
 
 import 'strings.dart';
@@ -63,26 +64,50 @@ class _MainContainerState extends State<MainContainer> {
             margin: EdgeInsetsDirectional.only(start: 10),
             child: Icon(
               IconFonts.heartbeat,
-              size: 40,
+              size: ScreenUtil().setHeight(40),
               color: Colors.redAccent,
             ),
           ),
-          title: Text(Strings.appName),
+          title: Text(
+            Strings.appName,
+            style: TextStyle(
+                fontSize: ScreenUtil().setSp(22),
+                fontWeight: FontWeight.bold,
+                foreground: Paint()
+                  ..shader = LinearGradient(
+                    colors: [Colors.redAccent, Theme.of(context).primaryColor],
+                  ).createShader(Rect.fromLTWH(0.0, 0.0, 300.0, 100.0))),
+          ),
           actions: [
             IconButton(icon: Icon(IconFonts.bell_alt), onPressed: () {})
           ],
         ),
-        bottomNavigationBar: TabBar(
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicatorColor: Theme.of(context).primaryColor,
-          indicator: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10))),
-          labelColor: Colors.white,
-          unselectedLabelColor: Theme.of(context).primaryColor,
-          tabs: _tabs,
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: clBackgroud,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.shade400,
+                  offset: Offset(0.0, 0.5),
+                  blurRadius: 20.0)
+            ],
+          ),
+          child: TabBar(
+            labelPadding: EdgeInsetsDirectional.only(top: 5),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorColor: Theme.of(context).primaryColor,
+            indicator: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Colors.redAccent, Theme.of(context).primaryColor]),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10))),
+            labelColor: Colors.white,
+            unselectedLabelColor: Theme.of(context).primaryColor,
+            tabs: _tabs,
+          ),
         ),
         body: TabBarView(
           physics: NeverScrollableScrollPhysics(),
