e4b8ccc3fc052840c7b26d130b1b7610621b7474
chucvv <vanchucv@gmail.com>
2021-02-12 23:05:59 +0700

    [Splash] Add support rive

--- features/lib/src/splash/presentation/ui/screen/splash_screen.dart
+++ features/lib/src/splash/presentation/ui/screen/splash_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -8,12 +8,14 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Artboard _riveArtboard;
+  RiveAnimationController _controller;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Image(image: AssetImage("assets/launcher/ic_launcher.png")),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Rive(artboard: _riveArtboard),
       ),
     );
   }
@@ -21,8 +23,26 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    /*Timer(Duration(seconds: 3), () {
       Navigator.popAndPushNamed(context, '/main');
-    });
+    });*/
+    // Load the animation file from the bundle, note that you could also
+    // download this. The RiveFile just expects a list of bytes.
+    rootBundle.load('assets/heart.riv').then(
+      (data) async {
+        final file = RiveFile();
+
+        // Load the RiveFile from the binary data.
+        if (file.import(data)) {
+          // The artboard is the root of the animation and gets drawn in the
+          // Rive widget.
+          final artboard = file.mainArtboard;
+          // Add a controller to play back a known animation on the main/default
+          // artboard.We store a reference to it so we can toggle playback.
+          artboard.addController(_controller = SimpleAnimation('heart'));
+          setState(() => _riveArtboard = artboard);
+        }
+      },
+    );
   }
 }
