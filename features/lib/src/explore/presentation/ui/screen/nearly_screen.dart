ae87f7bb47c067d92a3701aa06f4d9c9c352ef1b
chucvv <vanchucv@gmail.com>
2021-02-15 11:10:43 +0700

    [Map] Add view type

--- features/lib/src/explore/presentation/ui/screen/nearly_screen.dart
+++ features/lib/src/explore/presentation/ui/screen/nearly_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fimber/flutter_fimber.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NearlyScreen extends StatefulWidget {
@@ -12,6 +13,8 @@ class _NearlyScreenState extends State<NearlyScreen>
   GoogleMapController mapController;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  MapType _currentMapType = MapType.normal;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -30,13 +33,42 @@ class _NearlyScreenState extends State<NearlyScreen>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: _currentMapType,
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80.0, right: 10.0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: () => {_onMapTypeButtonPressed()},
+                materialTapTargetSize: MaterialTapTargetSize.padded,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.map_outlined,
+                  size: ScreenUtil().setWidth(20),
+                  color: Colors.black87.withOpacity(0.7),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
   }
 }
