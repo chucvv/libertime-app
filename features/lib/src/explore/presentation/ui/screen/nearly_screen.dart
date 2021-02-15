60f4cc8738a3cc7cd4fc6222c2f555b46486ef4d
chucvv <vanchucv@gmail.com>
2021-02-15 20:48:58 +0700

    [Map] Support my location

--- features/lib/src/explore/presentation/ui/screen/nearly_screen.dart
+++ features/lib/src/explore/presentation/ui/screen/nearly_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fimber/flutter_fimber.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class NearlyScreen extends StatefulWidget {
   @override
@@ -10,14 +11,25 @@ class NearlyScreen extends StatefulWidget {
 
 class _NearlyScreenState extends State<NearlyScreen>
     with AutomaticKeepAliveClientMixin<NearlyScreen> {
-  GoogleMapController mapController;
+  GoogleMapController _mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(10.779785, 106.699020);
+
+  final Location _location = Location();
 
   MapType _currentMapType = MapType.normal;
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    _mapController = controller;
+    _location.onLocationChanged.listen((event) {
+      Fimber.d('onLocationChanged (${event.latitude}, ${event.longitude})');
+      _mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(event.latitude, event.longitude), zoom: 11),
+        ),
+      );
+    });
   }
 
   @override
@@ -38,19 +50,22 @@ class _NearlyScreenState extends State<NearlyScreen>
           GoogleMap(
             mapType: _currentMapType,
             onMapCreated: _onMapCreated,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
             initialCameraPosition: CameraPosition(
               target: _center,
               zoom: 11.0,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(bottom: 80.0, right: 10.0),
+            padding: const EdgeInsets.only(top: 60.0, right: 10.0),
             child: Align(
-              alignment: Alignment.bottomRight,
+              alignment: Alignment.topRight,
               child: FloatingActionButton(
+                mini: true,
                 onPressed: () => {_onMapTypeButtonPressed()},
                 materialTapTargetSize: MaterialTapTargetSize.padded,
-                backgroundColor: Colors.white,
+                backgroundColor: Colors.white60,
                 child: Icon(
                   Icons.map_outlined,
                   size: ScreenUtil().setWidth(20),
